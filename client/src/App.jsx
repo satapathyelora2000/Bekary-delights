dfd25a15f22e62008fbdccc59ba56d7018379a80
Abhijit Pradhan <Abhijit986@>
2025-11-12 11:53:07 +0530

    Making frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"   // make sure jsx/tsx are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
